b89d28946eba120728634063559647fc71eac659
dhruvin-purohit <TheLordDhruvin@gmail.com>
2022-01-20 14:15:10 +0530

    Removed border from cursor, Looks beautiful now

--- components/Frame/Cursor.tsx
+++ components/Frame/Cursor.tsx
@@ -1,8 +1,37 @@
+import { useEffect } from "react";
+
 export default function Cursor() {
+  useEffect(() => {
+    const cursor = document.querySelector<HTMLElement>("#cursor")!;
+    const trail = document.querySelector<HTMLElement>("#cursor-trail")!;
+    document.addEventListener("mousemove", (e) => {
+      cursor.style.left = e.clientX + "px";
+      cursor.style.top = e.clientY + "px";
+      setTimeout(() => {
+        trail.style.left = e.clientX + "px";
+        trail.style.top = e.clientY + "px";
+      }, 200);
+
+      document.addEventListener("click", (e) => {
+        cursor.classList.add("scale-[2]");
+        trail.classList.add("scale-[2]");
+        setTimeout(() => {
+          cursor.classList.remove("scale-[2]");
+          trail.classList.remove("scale-[2]");
+        }, 750);
+      });
+    });
+  }, []);
   return (
     <>
-      <div id="cursor" className="fixed w-1 h-1 transition duration-500 -translate-x-1/2 -translate-y-1/2 bg-white rounded-full pointer-events-none z-[1]"></div>
-      <div id="cursor-trail" className="fixed w-8 h-8 transition duration-500 -translate-x-1/2 -translate-y-1/2 bg-white bg-opacity-25 border-2 border-white rounded-full pointer-events-none z-[1]"></div>
+      <div
+        id="cursor"
+        className="fixed w-1 h-1 transition duration-500 -translate-x-1/2 -translate-y-1/2 bg-white rounded-full pointer-events-none z-[1]"
+      ></div>
+      <div
+        id="cursor-trail"
+        className="fixed w-8 h-8 transition duration-500 -translate-x-1/2 -translate-y-1/2 bg-white bg-opacity-25 rounded-full pointer-events-none z-[1]"
+      ></div>
     </>
   );
 }
--- components/Frame/index.tsx
+++ components/Frame/index.tsx
@@ -1,30 +1,9 @@
 import NavBar from "./NavBar";
 import Footer from "./Footer";
-import { ReactElement, useEffect } from "react";
+import { ReactElement } from "react";
 import Cursor from "./Cursor";
 
 export default function Frame({ children }: { children: ReactElement }) {
-  useEffect(() => {
-    const cursor = document.querySelector<HTMLElement>("#cursor")!
-    const trail = document.querySelector<HTMLElement>("#cursor-trail")!
-    document.addEventListener("mousemove", (e) => {
-      cursor.style.left = e.clientX + "px";
-      cursor.style.top = e.clientY + "px";
-      setTimeout(() => {
-        trail.style.left = e.clientX + "px";
-        trail.style.top = e.clientY + "px";
-      }, 200)
-
-      document.addEventListener("click", (e) => {
-        cursor.classList.add("scale-[2]")
-        trail.classList.add("scale-[2]")
-        setTimeout(() => {
-          cursor.classList.remove("scale-[2]")
-          trail.classList.remove("scale-[2]")
-        }, 750)
-      })
-    })
-  },[])
 
   return (
     <>
